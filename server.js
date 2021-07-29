be499d6de91b6465e8a062e27496679630c1bba7
ahmed66 <ahmedmeshaal66@gmail.com>
2021-07-29 10:46:52 +0300

    Setup Express and Sequelize (Environment)

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+
+    var allowedOrigins = ['http://localhost:8081', 'http://localhost'];
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+const db = require('./app/config/db.config.js');
+
+
+// Create a Server
+var server = app.listen(8081, function () {
+
+    let host = server.address().address
+    let port = server.address().port
+
+    console.log("App listening at http://%s:%s", host, port);
+})
